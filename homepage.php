1fce3db363291cf71d543317c76380c42911b613
chiefdondjeu <florentdondjeu@yahoo.com>
2021-09-16 14:38:45 -0400

    add course btn appends new course to course table

--- homepage.php
+++ homepage.php
@@ -38,7 +38,80 @@
 				<p>to be dashboard</p>
 			</div>
 			<div class="schedule-new">
-				<p>new schedule</p>
+				<form action="" id="programplanningworksheet">
+					<h3 style="text-align: center; margin-bottom:20px;">Program Planning Worksheet</h3>
+					<label for="studentname">Name: </label>
+					<input type="text" id="studentname" name="studentname" size="40">
+					<label for="studentid">Id: </label>
+					<input type="text" id="studentid" name="studentid" maxlength="7" minlength="7" size="8">
+					<br>
+					
+					<label for="major">Major: </label>
+					<select id="major" id="major">
+						<!-- TO-DO -->
+					</select>
+
+					<label for="minor">Minor: </label>
+					<select id="minor" name="minor">
+						<!-- TO-DO -->
+					</select>
+
+					<br>
+
+					<span>Registering for</span>
+					<input type="radio" id="Fall" name="season" value="Fall">
+					<label for="Fall">Fall </label>
+					<input type="radio" id="Winter" name="season" value="Winter">
+					<label for="Winter">Winter </label>
+					<input type="radio" id="Spring" name="season" value="Spring">
+					<label for="Spring">Spring </label>
+					<input type="radio" id="Summer" name="season" value="Summer">
+					<label for="Summer">Summer </label>
+					
+					<label for="year">Year </label>
+					<input type="text" id="year" name="year" maxlength="4" minlength="4" size="6">
+					<br>
+
+					<span>Earned: </span> 
+					<input type="text" id="creditearned" name="creditearned" maxlength="3" size="4" readonly>
+					<span>credits</span>
+					<span>Credits</span>
+					<input type="text" id="creditenrolled" name="creditenrolled" size="3" value="0" readonly>
+					<span>currently enrolled in</span>
+					<br>
+
+					<!-- Course table goes here -->
+					<div id="schedule-course">
+						<table id="schedulecoursetable">
+							<tr>
+								<b>
+									<th>Course Number</th>
+									<th>Title</th>
+									<th>Credit</th>
+									<th>Major, Minor, Elective</th>
+									<th></th>
+								</b>
+							</tr>
+							<tr id="schedule-course-entry">
+								<th><input type='text' id="schedulecoursenumb" name="schedulecoursenumb" style="text-transform:uppercase"></th>
+								<th><input type='text' id="schedulecoursetitle" name="schedulecoursetitle"></th>
+								<th><input type='text' id="schedulecoursecredit" name="schedulecoursecredit"></th>
+								<th><input type='text' id="schedulecoursetype" name="schedulecoursetype"></th>
+								<th><button type='button' onclick="scheduleAddCourse(schedulecoursenumb.value, schedulecoursetitle.value, schedulecoursecredit.value, schedulecoursetype.value)">Add</button></th>
+								<!-- scheduleAddCourse(schedule-coursenumb.value, schedule-coursetitle.value, schedule-coursecredit.value, schedule-coursetype.value)" -->
+							</tr>
+						</table>
+					</div>
+					<br>
+
+					<label for="memo">Memo: </label><br>
+					<textarea rows="8" cols="50" name="memo" form="programplanningworksheet"></textarea>
+					<br>
+
+					<input type="submit" value="Submit">
+
+				</form>
+
 			</div>
 			<div class="schedule-view">
 				<p>view schedule</p>
@@ -59,6 +132,7 @@
 	<script>
 		$(document).ready(function () {
 			$('#content > *').not(':first').hide();
+			//$('#content > *').not('.schedule-new').hide();
 		});
 
 		// nav bar sub menu
@@ -99,6 +173,16 @@
 			$('#content > *').hide();
 			$('#content .contact-info').show();
 		});
+
+		function scheduleAddCourse(subj, ttl, cr, typ) {
+			// console.log(subj, ttl, cr, typ);
+			if (subj != "" && ttl != "" && cr != "") {
+				// but = '<button type="button" onclick="">Delete</botton>'
+				text = "<tr><th>" + subj + "</th><th>" + ttl + "</th><th>" + cr + "</th><th>" + typ + "</th><th></th></tr>";
+				$('#schedulecoursetable').append(text);
+				$("#schedulecoursetable :input").val("");
+			}
+		}
 	</script>
 	
 </body>
