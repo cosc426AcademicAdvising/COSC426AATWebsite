8d88802805a5cef2c7309256760f30f1104707b5
quincden <78387066+quincden@users.noreply.github.com>
2021-09-27 15:29:24 -0400

    Updated styling of dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -30,64 +30,106 @@
 
 		<div id="content">
 			<div class="container">
-			
 				<div class="schedule">
-				<h3> Upcoming Schedule </h3>
-				<table class="schedule_table">
-					<thead>
-						<tr>
-							<th>Course Number</th>
-							<th>Title</th>
-							<th>Credit</th>
-							<th>Program</a></th>
-						</tr>
-					</thead>
-					<?php
-						for($i=0;$i<count($student['taking_course']);$i++){
-							echo "<tr>";
-								echo "<td>".$student['taking_course'][$i]['subject']." ".$student['taking_course'][$i]['catalog']."</td>";
-								echo "<td>".$student['taking_course'][$i]['title']."</td>";
-								echo "<td>".$student['taking_course'][$i]['cred']."</td>";
-								echo "<td>".$student['taking_course'][$i]['genED']."</td>";
-							echo "</tr>";
-						}
-					?>
-				</table>
+					<h3> Upcoming Schedule </h3>
+					<table class="schedule_table">
+						<thead>
+							<tr>
+								<th>Course Number</th>
+								<th>Title</th>
+								<th>Credit</th>
+								<th>Program</a></th>
+							</tr>
+						</thead>
+						<?php
+							for($i=0;$i<count($student['taking_course']);$i++){
+								echo "<tr>";
+									echo "<td>".$student['taking_course'][$i]['subject']." ".$student['taking_course'][$i]['catalog']."</td>";
+									echo "<td>".$student['taking_course'][$i]['title']."</td>";
+									echo "<td>".$student['taking_course'][$i]['cred']."</td>";
+									echo "<td>".$student['taking_course'][$i]['genED']."</td>";
+								echo "</tr>";
+							}
+						?>
+					</table>
 				</div>
 				<div class="plan">
-				<h3> Major Plan Schedule</h3>
-				<table class="schedule_table">
-					<thead>
-						<tr>
-							<th>Course Number</th>
-							<th>Title</th>
-							<th>Credit</th>
-						</tr>
-					</thead>
-					<?php
-						for($i=0;$i<count($plan[$semester_plan]);$i++){
-							echo "<tr>";
-								echo "<td>".$plan[$semester_plan][$i]['subject']." ".$plan[$semester_plan][$i]['catalog']."</td>";
-								echo "<td>".$plan[$semester_plan][$i]['title']."</td>";
-								echo "<td>".$plan[$semester_plan][$i]['cred']."</td>";
-							echo "</tr>";
-						}
-					?>
-				</table>
+					<h3> Major Plan Schedule</h3>
+					<table class="schedule_table">
+						<thead>
+							<tr>
+								<th>Course Number</th>
+								<th>Title</th>
+								<th>Credit</th>
+							</tr>
+						</thead>
+						<?php
+							for($i=0;$i<count($plan[$semester_plan]);$i++){
+								echo "<tr>";
+									echo "<td>".$plan[$semester_plan][$i]['subject']." ".$plan[$semester_plan][$i]['catalog']."</td>";
+									echo "<td>".$plan[$semester_plan][$i]['title']."</td>";
+									echo "<td>".$plan[$semester_plan][$i]['cred']."</td>";
+								echo "</tr>";
+							}
+						?>
+					</table>
 				</div>
 				<div class="personal_info">
-					<h3>Personal Info</h3>
-					<div class="personal_info_fill">
-						<?php echo "<h4> Student ID: ".$student['s_id']."</h4>"; ?>
-						<?php echo "<h4> Current Year: ".$student['year']."</h4>"; ?>
-						<?php echo "<h4> Current Semester: ".$student['semester']."</h4>"; ?>
-						<?php echo "<h4> Advisor Email: ".$student['advisor_mail']."</h4>"; ?>
+					<h3> Personal Information </h3>
+					<div class="dash_info">
+						<?php 
+							echo "<h4> Name: ".$student['name']."</h4>";
+							echo "<h4> Student ID: ".$student['s_id']."</h4>";
+							$cnt = count($student['major']);
+							if($cnt == 1)
+								$major_print = "<h4> Major(s): ".$student['major'][0]['title']."</h4>";
+							else if($cnt == 2)
+								$major_print = "<h4> Major(s): ".$student['major'][0]['title'].", ".$student['major'][1]['title']."</h4>";
+							else if($cnt == 3)
+								$major_print = "<h4> Major(s): ".$student['major'][0]['title'].", ".$student['major'][1]['title'].", ".$student['major'][2]['title']."</h4>";
+							echo $major_print;
+							$cnt = count($student['minor']);
+							if($cnt == 1)
+								$major_print = "<h4> Minor(s): ".$student['minor'][0]['title']."</h4>";
+							else if($cnt == 2)
+								$major_print = "<h4> Minor(s): ".$student['minor'][0]['title'].", ".$student['minor'][1]['title']."</h4>";
+							else if($cnt == 3)
+								$major_print = "<h4> Minor(s): ".$student['minor'][0]['title'].", ".$student['minor'][1]['title'].", ".$student['minor'][2]['title']."</h4>";
+							echo $major_print;
+						?>
+						<br>
 						<h4> Change Password Button</h4>
 					</div>
-
+				</div>
+				<div class="credit_info">
+					<h3> Credit Info </h3>
+					<div class="dash_info">
+						<?php
+							echo "<h4> Current Year: ".$student['year']."</h4>";
+							echo "<h4> Current Semester: ".$student['semester']."</h4>";
+							
+							$credits = 0;
+							for($i=0;$i<count($student['taking_course']);$i++)
+								$credits = $credits+$student['taking_course'][$i]['cred'];
+							
+							echo "<h4> Credits Registered For: ".$credits."</h4>"; 
+							echo "<h4> Total Credits: ".$student['credits']."</h4>"; 
+						?>
+					</div>
+					
+				</div>
+				<div class="advisor_info">
+					<h3>Enrollment Info </h3>
+					<div class="dash_info">
+						<?php 
+							echo "<h4> Advisor Email: ".$student['advisor_mail']."</h4>"; 
+							echo "<h4> Enrollment Date: ".$student['enrll']."</h4>";
+							echo "<h4> Registering For: ".$student['registering_for']."</h4>";
+						?>
+					</div>
 				</div>
 				<div class="progress_bar">
-					<header>Progress Bar</header>
+					<h4>Progress Bar</h4>
 				</div>
 			</div>
 		</div>
