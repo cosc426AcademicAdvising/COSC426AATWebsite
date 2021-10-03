1fb70046528a41f18b523e7a0f93814b582ce288
quincden <78387066+quincden@users.noreply.github.com>
2021-10-03 15:39:56 -0400

    Added Buttons to dashboard.php
    The Recommended courses has button for each of their majors.  Only displays correctly for triple majors need to edit width for double and single.
    Added new edit and view buttons above the current schedule.

--- dashboard.php
+++ dashboard.php
@@ -24,6 +24,10 @@
 		$progress_percent = intval(100 * ($progress / 120));
 		$progress_percent_str = strval($progress_percent)."%";
 		$progress_width = 'width: '.$progress_percent.'%;';
+		$majors = array();
+		for($i=0;$i<count($student['major']);$i++) {
+			$majors[$i] = $student['major'][$i]['title'];
+		}
 	?>
 	<style>
 			.progress {
@@ -43,13 +47,24 @@
 					<div class="dash_header">
 						<h3> Upcoming Schedule </h3>
 					</div>
+					<div class="tabs_area">
+						<div class="tabs">
+						<form method="post">
+						<?php
+								echo "<button class='fyp_tabs' name='new' value='new'>New</button>";
+								echo "<button class='fyp_tabs' name='edit' value='edit'>Edit</button>";
+								echo "<button class='fyp_tabs' name='view' value='view'>View</button>";
+						?>
+						</form>
+						</div>
+					</div>	
 					<div class="table_area">
 						<table class="schedule_table">
 							<thead>
 								<tr>
-									<th style="border-radius: 10px 0px 0px 0px;">Course Number</th>
+									<th style="border-radius: 0px 0px 0px 0px;">Course Number</th>
 									<th>Title</th>
-									<th style="border-radius: 0px 10px 0px 0px;">Credit</th>
+									<th style="border-radius: 0px 0px 0px 0px;">Credit</th>
 								</tr>
 							</thead>
 							<?php
@@ -82,41 +97,31 @@
 				<div class="plan" id="plan">
 					<div class="dash_header">
 						<h3> Recommended Courses</h3>
+					
+					
+					</div>
+					<div class="tabs_area">
+						<div class="tabs">
+						<form method="post">
+						<?php
+							for($i=0;$i<count($majors);$i++)
+								echo "<button class='fyp_tabs' name='major' value=".$majors[$i].">".$majors[$i]."</button>";
+						?>
+						</form>
+						</div>
 					</div>
 					<div class="table_area">
 						<table class="schedule_table">
-							<thead>
-								<tr>
-									<th style="border-radius: 10px 0px 0px 0px;">Course Number</th>
-									<th>Title</th>
-									<th style="border-radius: 0px 10px 0px 0px;">Credit</th>
-								</tr>
-							</thead>
+							
 							<?php
-								for($i=0;$i<count($plan[$semester_plan]);$i++){
-									echo "<tr>";
-										if($i+1 == count($plan[$semester_plan])) {
-											$style_subject = 'border-left: none; border-bottom: none;';
-											$style_title = 'border-bottom: none;';
-											$style_credit = 'border-right: none; border-bottom: none;';
-										}
-										else {
-											$style_subject = 'border-left: none;';
-											$style_title = '';
-											$style_credit = 'border-right: none;';
-										}
-										if( $i % 2 != 0) {
-											$style_subject = $style_subject.'background-color: #c9c9c9';
-											$style_title = $style_title.'background-color: #c9c9c9';
-											$style_credit = $style_credit.'background-color: #c9c9c9';
-										}
-										echo "<td style='$style_subject'>".$plan[$semester_plan][$i]['subject']." ".$plan[$semester_plan][$i]['catalog']."</td>";
-										echo "<td style='$style_title'>".$plan[$semester_plan][$i]['title']."</td>";
-										echo "<td style='$style_credit'>".$plan[$semester_plan][$i]['cred']."</td>";
-									echo "</tr>";
-								}
+								include_once 'funcs/FourYearFunctions.php';
+								if(isset($_POST['major']))
+									$plan = getFourYearbyMajor($_POST['major']);
+								if($plan != "")
+									displayFourYearSemester($plan, $semester_plan);
 							?>
 						</table>
+						
 					</div>
 				</div>
 				<div class="personal_info" id="personal_info">
@@ -125,21 +130,21 @@
 					</div>
 					<div class="info_area">
 						<div class="dash_info">
-							<?php
+							<?php 
 								echo "<div class='row_odd'>";
-								echo "<div class='row_odd_field'>";
+								echo "<div class='row_odd_field' id='row_odd_field'>";
 								echo "<h4> Name </h4>";
 								echo "</div>";
-								echo "<div class='row_odd_value'>";
+								echo "<div class='row_odd_value' id='row_odd_value'>";
 								echo "<h4>".$student['name']."</h4>";
 								echo "</div>";
 								echo "</div>";
 
-								echo "<div class='row_even'>";
-								echo "<div class='row_even_field'>";
+								echo "<div class='row_even' >";
+								echo "<div class='row_even_field' id='row_even_field'>";
 								echo "<h4> Student ID</h4>";
 								echo "</div>";
-								echo "<div class='row_even_value'>";
+								echo "<div class='row_even_value' id='row_even_value'>";
 								echo "<h4>".$student['s_id']."</h4>";
 								echo "</div>";
 								echo "</div>";
@@ -158,10 +163,10 @@
 									$major_value_print = "<h4>".$student['major'][0]['title'].", ".$student['major'][1]['title'].", ".$student['major'][2]['title']."</h4>";
 								}
 								echo "<div class='row_odd'>";
-								echo "<div class='row_odd_field'>";
+								echo "<div class='row_odd_field' id='row_odd_field'>";
 								echo $major_field_print;
-								echo "</div>";
-								echo "<div class='row_odd_value'>";
+								echo "</div>";	
+								echo "<div class='row_odd_value' id='row_odd_value'>";
 								echo $major_value_print;
 								echo "</div>";
 								echo "</div>";
@@ -181,10 +186,10 @@
 								}
 
 								echo "<div class='row_even' style='border-radius: 0px 0px 10px 10px;'>";
-								echo "<div class='row_even_field' style='border-bottom: none;'>";
+								echo "<div class='row_even_field' id='row_even_field' style='border-bottom: none;'>";
 								echo $minor_field_print;
 								echo "</div>";
-								echo "<div class='row_even_value' style='border-bottom: none;'>";
+								echo "<div class='row_even_value' id='row_even_value' style='border-bottom: none;'>";
 								echo $minor_value_print;
 								echo "</div>";
 								echo "</div>";
@@ -200,19 +205,19 @@
 						<div class="dash_info">
 							<?php
 								echo "<div class='row_odd'>";
-								echo "<div class='row_odd_field'>";
+								echo "<div class='row_odd_field' id='row_odd_field'>";
 								echo "<h4> Current Year</h4>";
 								echo "</div>";
-								echo "<div class='row_odd_value'>";
+								echo "<div class='row_odd_value' id='row_odd_value'>";
 								echo "<h4>".$student['year']."</h4>";
 								echo "</div>";
 								echo "</div>";
 
 								echo "<div class='row_even'>";
-								echo "<div class='row_even_field'>";
+								echo "<div class='row_even_field' id='row_even_field'>";
 								echo "<h4> Current Semester</h4>";
 								echo "</div>";
-								echo "<div class='row_even_value'>";
+								echo "<div class='row_even_value' id='row_even_value'>";
 								echo "<h4>".$student['semester']."</h4>";
 								echo "</div>";
 								echo "</div>";
@@ -222,20 +227,20 @@
 									$credits = $credits+$student['taking_course'][$i]['cred'];
 
 								echo "<div class='row_odd'>";
-								echo "<div class='row_odd_field'>";
+								echo "<div class='row_odd_field' id='row_odd_field'>";
 								echo "<h4> Credits Registered For</h4>";
 								echo "</div>";
-								echo "<div class='row_odd_value'>";
-								echo "<h4>".$credits."</h4>";
+								echo "<div class='row_odd_value' id='row_odd_value'>";
+								echo "<h4>".$credits."</h4>"; 
 								echo "</div>";
 								echo "</div>";
-
+								
 								echo "<div class='row_even' style='border-radius: 0px 0px 10px 10px;'>";
-								echo "<div class='row_even_field' style='border-bottom: none'>";
+								echo "<div class='row_even_field' id='row_even_field' style='border-bottom: none'>";
 								echo "<h4> Total Credits</h4>";
 								echo "</div>";
-								echo "<div class='row_even_value' style='border-bottom: none'>";
-								echo "<h4>".$student['credits']."</h4>";
+								echo "<div class='row_even_value' id='row_even_value' style='border-bottom: none'>";
+								echo "<h4>".$student['credits']."</h4>"; 
 								echo "</div>";
 								echo "</div>";
 							?>
@@ -248,30 +253,30 @@
 					</div>
 					<div class="info_area">
 						<div class="dash_info">
-							<?php
+							<?php 
 								echo "<div class='row_odd'>";
-								echo "<div class='row_odd_field'>";
+								echo "<div class='row_odd_field' id='row_odd_field'>";
 								echo "<h4> Advisor Email</h4>";
 								echo "</div>";
-								echo "<div class='row_odd_value'>";
-								echo "<h4>".$student['advisor_mail']."</h4>";
+								echo "<div class='row_odd_value' id='row_odd_value'>";
+								echo "<h4>".$student['advisor_mail']."</h4>"; 
 								echo "</div>";
 								echo "</div>";
-
+								
 								echo "<div class='row_even'>";
-								echo "<div class='row_even_field'>";
+								echo "<div class='row_even_field' id='row_even_field'>";
 								echo "<h4> Enrollment Date</h4>";
 								echo "</div>";
-								echo "<div class='row_even_value' >";
+								echo "<div class='row_even_value' id='row_even_value'>";
 								echo "<h4>".$student['enrll']."</h4>";
 								echo "</div>";
 								echo "</div>";
-
+								
 								echo "<div class='row_odd' style='border-radius: 0px 0px 10px 10px;'>";
-								echo "<div class='row_odd_field' style='border-bottom: none'>";
+								echo "<div class='row_odd_field' id='row_odd_field' style='border-bottom: none'>";
 								echo "<h4> Registering For</h4>";
 								echo "</div>";
-								echo "<div class='row_odd_value' style='border-bottom: none'>";
+								echo "<div class='row_odd_value' id='row_odd_value' style='border-bottom: none'>";
 								echo "<h4>".$student['registering_for']."</h4>";
 								echo "</div>";
 								echo "</div>";
@@ -302,57 +307,189 @@
 				var cont_w = w*.8;
 				var cont_h = h-75;
 
-				document.getElementById("container").style.width=w*.8+"px";
-				document.getElementById("container").style.height=h-75+"px";
+				if(w > 1280 || h > 720) {
+					document.getElementById("container").style.width=w*.8+"px";
+					document.getElementById("container").style.height=h-75+"px";
 
-				document.getElementById("schedule").style.width=cont_w*.475+"px";
-				document.getElementById("schedule").style.height=cont_h*.3+"px";
+					document.getElementById("schedule").style.width=cont_w*.475+"px";
+					document.getElementById("schedule").style.height=cont_h*.4+"px";
 
-				document.getElementById("plan").style.width=cont_w*.475+"px";
-				document.getElementById("plan").style.height=cont_h*.3+"px";
+					document.getElementById("plan").style.width=cont_w*.475+"px";
+					document.getElementById("plan").style.height=cont_h*.4+"px";
 
-				document.getElementById("personal_info").style.width=cont_w*.3075+"px";
-				document.getElementById("personal_info").style.height=cont_h*.3+"px";
+					document.getElementById("personal_info").style.width=cont_w*.3075+"px";
+					document.getElementById("personal_info").style.height=cont_h*.25+"px";
 
-				document.getElementById("advisor_info").style.width=cont_w*.3075+"px";
-				document.getElementById("advisor_info").style.height=cont_h*.3+"px";
+					document.getElementById("advisor_info").style.width=cont_w*.3075+"px";
+					document.getElementById("advisor_info").style.height=cont_h*.25+"px";
 
-				document.getElementById("credit_info").style.width=cont_w*.3075+"px";
-				document.getElementById("credit_info").style.height=cont_h*.3+"px";
+					document.getElementById("credit_info").style.width=cont_w*.3075+"px";
+					document.getElementById("credit_info").style.height=cont_h*.25+"px";
 
-				document.getElementById("progress_bar").style.width=cont_w*.975+"px";
-				document.getElementById("progress_bar").style.height=cont_h*.15+"px";
+					document.getElementById("progress_bar").style.width=cont_w*.975+"px";
+					document.getElementById("progress_bar").style.height=cont_h*.15+"px";
+
+					var evf = document.getElementsByClassName("row_even_field");
+					var l = evf.length;
+					for(var i=0;i<l;i++)
+						evf[i].style.fontSize='14px';
+					var evv = document.getElementsByClassName("row_even_value");
+					var l = evv.length;
+					for(var i=0;i<l;i++)
+						evv[i].style.fontSize='14px';
+					var odf = document.getElementsByClassName("row_odd_field");
+					var l = odf.length;
+					for(var i=0;i<l;i++)
+						odf[i].style.fontSize='14px';
+					var odv = document.getElementsByClassName("row_odd_value");
+					var l = odv.length;
+					for(var i=0;i<l;i++)
+						odv[i].style.fontSize='14px';
+				}
+				else {
+					var w_tmp = 1280;
+					var h_tmp = 720;
+
+					var cont_w = w_tmp*.8;
+					var cont_h = h_tmp-75;
+
+					document.getElementById("container").style.width=w_tmp*.8+"px";
+					document.getElementById("container").style.height=h_tmp-75+"px";
+
+					document.getElementById("schedule").style.width=cont_w*.475+"px";
+					document.getElementById("schedule").style.height=cont_h*.4+"px";
+
+					document.getElementById("plan").style.width=cont_w*.475+"px";
+					document.getElementById("plan").style.height=cont_h*.4+"px";
+
+					document.getElementById("personal_info").style.width=cont_w*.3075+"px";
+					document.getElementById("personal_info").style.height=cont_h*.25+"px";
+
+					document.getElementById("advisor_info").style.width=cont_w*.3075+"px";
+					document.getElementById("advisor_info").style.height=cont_h*.25+"px";
+
+					document.getElementById("credit_info").style.width=cont_w*.3075+"px";
+					document.getElementById("credit_info").style.height=cont_h*.25+"px";
+
+					document.getElementById("progress_bar").style.width=cont_w*.975+"px";
+					document.getElementById("progress_bar").style.height=cont_h*.15+"px";
+
+					var evf = document.getElementsByClassName("row_even_field");
+					var l = evf.length;
+					for(var i=0;i<l;i++)
+						evf[i].style.fontSize='12px';
+					var evv = document.getElementsByClassName("row_even_value");
+					var l = evv.length;
+					for(var i=0;i<l;i++)
+						evv[i].style.fontSize='12px';
+					var odf = document.getElementsByClassName("row_odd_field");
+					var l = odf.length;
+					for(var i=0;i<l;i++)
+						odf[i].style.fontSize='12px';
+					var odv = document.getElementsByClassName("row_odd_value");
+					var l = odv.length;
+					for(var i=0;i<l;i++)
+						odv[i].style.fontSize='12px';
+
+				}
 			}
-/*
+
 			window.onresize = function() {
 				var w = window.innerWidth;
 				var h = window.innerHeight;
 
 				var cont_w = w*.8;
 				var cont_h = h-75;
+				
+				if(w > 1700) {
+					document.getElementById("container").style.width=w*.8+"px";
+					document.getElementById("schedule").style.width=cont_w*.475+"px";
+					document.getElementById("plan").style.width=cont_w*.475+"px";
+					document.getElementById("personal_info").style.width=cont_w*.3075+"px";
+					document.getElementById("advisor_info").style.width=cont_w*.3075+"px";
+					document.getElementById("credit_info").style.width=cont_w*.3075+"px";
+					document.getElementById("progress_bar").style.width=cont_w*.975+"px";
 
-				document.getElementById("container").style.width=w*.8+"px";
-				document.getElementById("container").style.height=h-75+"px";
+					var evf = document.getElementsByClassName("row_even_field");
+					var l = evf.length;
+					for(var i=0;i<l;i++)
+						evf[i].style.fontSize='14px';
+					var evv = document.getElementsByClassName("row_even_value");
+					var l = evv.length;
+					for(var i=0;i<l;i++)
+						evv[i].style.fontSize='14px';
+					var odf = document.getElementsByClassName("row_odd_field");
+					var l = odf.length;
+					for(var i=0;i<l;i++)
+						odf[i].style.fontSize='14px';
+					var odv = document.getElementsByClassName("row_odd_value");
+					var l = odv.length;
+					for(var i=0;i<l;i++)
+						odv[i].style.fontSize='14px';
+				}
+				else {
+					var evf = document.getElementsByClassName("row_even_field");
+					var l = evf.length;
+					for(var i=0;i<l;i++)
+						evf[i].style.fontSize='12px';
+					var evv = document.getElementsByClassName("row_even_value");
+					var l = evv.length;
+					for(var i=0;i<l;i++)
+						evv[i].style.fontSize='12px';
+					var odf = document.getElementsByClassName("row_odd_field");
+					var l = odf.length;
+					for(var i=0;i<l;i++)
+						odf[i].style.fontSize='12px';
+					var odv = document.getElementsByClassName("row_odd_value");
+					var l = odv.length;
+					for(var i=0;i<l;i++)
+						odv[i].style.fontSize='12px';
+				}
+				if(h > 720) {
+					document.getElementById("container").style.height=h-75+"px";
+					document.getElementById("schedule").style.height=cont_h*.4+"px";
+					document.getElementById("plan").style.height=cont_h*.4+"px";
+					document.getElementById("personal_info").style.height=cont_h*.25+"px";
+					document.getElementById("advisor_info").style.height=cont_h*.25+"px";
+					document.getElementById("credit_info").style.height=cont_h*.25+"px";
+					document.getElementById("progress_bar").style.height=cont_h*.15+"px";
 
-				document.getElementById("schedule").style.width=cont_w*.475+"px";
-				document.getElementById("schedule").style.height=cont_h*.325+"px";
-
-				document.getElementById("plan").style.width=cont_w*.475+"px";
-				document.getElementById("plan").style.height=cont_h*.325+"px";
-
-				document.getElementById("personal_info").style.width=cont_w*.3075+"px";
-				document.getElementById("personal_info").style.height=cont_h*.325+"px";
-
-				document.getElementById("advisor_info").style.width=cont_w*.3075+"px";
-				document.getElementById("advisor_info").style.height=cont_h*.325+"px";
-
-				document.getElementById("credit_info").style.width=cont_w*.3075+"px";
-				document.getElementById("credit_info").style.height=cont_h*.325+"px";
-
-				document.getElementById("progress_bar").style.width=cont_w*.975+"px";
-				document.getElementById("progress_bar").style.height=cont_h*.15+"px";
+					var evf = document.getElementsByClassName("row_even_field");
+					var l = evf.length;
+					for(var i=0;i<l;i++)
+						evf[i].style.fontSize='14px';
+					var evv = document.getElementsByClassName("row_even_value");
+					var l = evv.length;
+					for(var i=0;i<l;i++)
+						evv[i].style.fontSize='14px';
+					var odf = document.getElementsByClassName("row_odd_field");
+					var l = odf.length;
+					for(var i=0;i<l;i++)
+						odf[i].style.fontSize='14px';
+					var odv = document.getElementsByClassName("row_odd_value");
+					var l = odv.length;
+					for(var i=0;i<l;i++)
+						odv[i].style.fontSize='14px';
+				}
+				else {
+					var evf = document.getElementsByClassName("row_even_field");
+					var l = evf.length;
+					for(var i=0;i<l;i++)
+						evf[i].style.fontSize='14px';
+					var evv = document.getElementsByClassName("row_even_value");
+					var l = evv.length;
+					for(var i=0;i<l;i++)
+						evv[i].style.fontSize='14px';
+					var odf = document.getElementsByClassName("row_odd_field");
+					var l = odf.length;
+					for(var i=0;i<l;i++)
+						odf[i].style.fontSize='14px';
+					var odv = document.getElementsByClassName("row_odd_value");
+					var l = odv.length;
+					for(var i=0;i<l;i++)
+						odv[i].style.fontSize='14px';
+				}
 			}
-		*/
 		</script>
 	</body>
 	</html>
