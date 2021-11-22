ea834760cf71b409d37b48eac5606ed7f9136763
Florent Dondjeu Tschoufack <florentdondjeu@yahoo.com>
2021-11-21 22:10:44 -0500

    renamed course_rec javascript file

--- scheduleNew.php
+++ scheduleNew.php
@@ -2,8 +2,9 @@
 <html lang="en">
 <head>
 	<title>Academic Planar</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<!-- for caret -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 	<link rel="stylesheet" href="CSS/nav.css">
@@ -224,7 +225,7 @@
 <script src="JS/scheduleNewFuncs.js"></script>
 
 <!-- courses recommandations -->
-<script src="JS/recommendedCourses.js"></script>
+<script src="JS/recommendCourses.js"></script>
 
 </body>
 </html>
