d23f9eab98ab61be3e82fee7d16ba7d64275fa39
quincden <78387066+quincden@users.noreply.github.com>
2021-11-29 18:38:26 -0500

    Fixed location address

--- app/views/login.php
+++ app/views/login.php
@@ -56,7 +56,7 @@
 					<button type = "submit" id="login" name = "login" style="margin-bottom: 20px;">Log In</button>
 				</form>
 				<hr>
-				<form action="newuser" method="post">
+				<form action="newUser" method="post">
 					<label for="SignUp" style="margin-top: 5px;">New User?</label><br>
 					<button type = "submit" style="margin-top: 10px;" id="SignUp" name = "SignUp">Sign Up</button>
 				</form>
--- app/views/newUser.php
+++ app/views/newUser.php
@@ -10,7 +10,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-	<link rel="stylesheet" href="CSS\newUser.css">
+	<link rel="stylesheet" href="public/css/newUser.css">
 </head>
 <body>
     <header>
