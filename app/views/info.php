874f6098b64d42f39ce4573bc4eb0b4083d0d43d
quincden <78387066+quincden@users.noreply.github.com>
2021-12-04 01:28:43 -0500

    Changed styling of info.php
    Fixed issue with header being displayed below table.
    Fixed table width
    Added alternating table row colors

--- app/views/info.php
+++ app/views/info.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<title>Academic Planar</title>
 	<meta charset="UTF-8">
@@ -13,81 +12,76 @@
 	<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
 
 	<?php
-	ob_start();
-	session_start();
-	// require 'vendor/autoload.php';
+		ob_start();
+		session_start();
+		require 'vendor/autoload.php';
 	?>
 </head>
 
 <body>
 	<?php
-	include 'nav.php';
-	// include_once 'funcs/StudentFunctions.php';
-	$student = getStudent($_SESSION['username']);
+		include 'nav.php';
+		$student = getStudent($_SESSION['username']);
 	?>
-	<div id="content">
-		<div id="container">
-			<div class='taking' id='taking'>
-				<div class='taking_header'>
-					<h3>Links and contact info</h3>
-				</div>
-				<div class='table_area'>
-					<table class='taking_table'>
-						<thead>
+		<div id="content">
+			<div id="container">
+				<div class='taking' id='taking'>
+					<div class='taking_header'>
+						<h3>Student Resources</h3>
+					</div>
+					<div class='table_area'>
+						<table class='taking_table'>
+							<thead>
+								<tr>
+									<th style="border-right: solid;">Service</th>
+									<th>Location</th>
+									<th style="border-left: solid;">Contact</th>
+								</tr>
+							</thead>
 							<tr>
-								<th style="border-radius: 10px 0px 0px 0px;">Service</th>
-								<th>Location</th>
-								<th style="border-radius: 0px 10px 0px 0px;">Number/email</th>
+							<td id='serv'>General advising</td>
+							<td id='loc'>Blackwell Hall</td>
+							<td id='con'>410-546-4366</td>
 							</tr>
-						</thead>
-						<tr>
-							<td>General advising</td>
-							<td>Blackwell Hall</td>
-							<td>410-546-4366</td>
-						</tr>
-						<tr>
-							<td>Office of the Registrar</td>
-							<td>Holloway Hall 120</td>
-							<td>registrar@salisbury.edu</td>
-						</tr>
-						<tr>
-							<td>Department Contacts</td>
-							<td>Select school and department for relevant contacts --></td>
-							<td><a href="www.salisbury.edu/academic-offices">Academic offices link</a></td>
-						</tr>
-						<tr>
-							<td>Center for Student Achievement</td>
-							<td>Academic Commons 270</td>
-							<td>410-677-4865</td>
-						</tr>
-						<tr>
-							<td>Counseling Center</td>
-							<td>Guerrieri Student Union 263</td>
-							<td>410-543-6070</td>
-						</tr>
-						<tr>
-							<td>Libraries</td>
-							<td>Guerreri Academic Commons</td>
-							<td>410-543-6130/libraries@salisbury.edu</td>
-						</tr>
-						<tr>
-							<td>IT help desk</td>
-							<td>Academic Commons 145</td>
-							<td>410-543-6070</td>
-						</tr>
-					</table>
+							<tr>
+							<td id='serv' style="background-color: #c9c9c9;">Office of the Registrar</td>
+							<td id='loc' style="background-color: #c9c9c9;">Holloway Hall 120</td>
+							<td id='con' style="background-color: #c9c9c9;">registrar@salisbury.edu</td>
+							</tr>
+							<tr>
+							<td id='serv'>Department Contacts</td>
+							<td id='loc'>Select School and Department for Relevant Contacts</td>
+							<td id='con'><a href="www.salisbury.edu/academic-offices">Academic offices link</a></td>
+							</tr>
+							<tr>
+							<td id='serv' style="background-color: #c9c9c9;">Center for Student Achievement</td>
+							<td id='loc' style="background-color: #c9c9c9;">Academic Commons 270</td>
+							<td id='con' style="background-color: #c9c9c9;">410-677-4865</td>
+							</tr>
+							<tr>
+							<td id='serv'>Counseling Center</td>
+							<td id='loc'>Guerrieri Student Union 263</td>
+							<td id='con'>410-543-6070</td>
+							</tr>
+							<tr>
+							<td id='serv' style="background-color: #c9c9c9;">Libraries</td>
+							<td id='loc' style="background-color: #c9c9c9;">Guerreri Academic Commons</td>
+							<td id='con' style="background-color: #c9c9c9;">410-543-6130/libraries@salisbury.edu</td>
+							</tr>
+							<tr>
+							<td id='serv' style="border-bottom: none;">IT help desk</td>
+							<td id='loc' style="border-bottom: none;">Academic Commons 145</td>
+							<td id='con' style="border-bottom: none;">410-543-6070</td>
+							</tr>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	</div> <!-- flexbox div ends -->
+</div> <!-- flexbox div ends -->
 
-	<script>
-		$('.contact-info-btn').css({
-			"color": "#8a0000",
-			"border-left-color": "#8a0000"
-		});
-	</script>
+<script>
+	$('.contact-info-btn').css({"color":"#8a0000","border-left-color":"#8a0000"});
+</script>
 </body>
-
 </html>
