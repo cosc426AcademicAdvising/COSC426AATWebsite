6778b45cd2aa4f314ddda52d5064b6abf3afa1f1
quincden <78387066+quincden@users.noreply.github.com>
2021-12-04 23:44:37 -0500

    Added styling to viewSchedule
    Applied CSS to viewSchedule that is consist with rest of website.

--- app/views/viewSchedule.php
+++ app/views/viewSchedule.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<title>Academic Planar</title>
 	<meta charset="UTF-8">
@@ -9,107 +8,102 @@
 	<!-- for caret -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 	<link rel="stylesheet" href="public/css/nav.css">
-	<link rel="stylesheet" href="public/css/viewSchedule.css">
+	<link rel="stylesheet" href="public/css/scheduleView.css">
 	<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
 
 	<?php
-	ob_start();
-	session_start();
-	// require 'vendor/autoload.php';
+		ob_start();
+		session_start();
 	?>
 </head>
 
 <body>
 	<?php
-	include 'nav.php';
-	// include_once 'funcs/StudentFunctions.php';
-	$student = getStudent($_SESSION['username']);
+		include 'nav.php';
+		$student = getStudent($_SESSION['username']);
 	?>
-	<div id="content">
-		<div id="container">
-			<div class='taking' id='taking'>
-				<div class='taking_header'>
-					<h3>Main Courses</h3>
+		<div id="content">
+			<?php
+				$total = count($student["backup_course"]);
+				if( $total < 2)
+					echo "<div class='container' id='container' style='grid-template-rows: 1fr .5fr;'>";
+				else
+					echo "<div class='container' id='container' style='grid-auto-rows: 1fr;'>";
+			?>
+				<div class='taking' id='taking'>
+					<div class='taking_header'>
+						<h3>Main Courses</h3>
+					</div>
+					<div class='table_area'>
+						<table class='taking_table'>
+							<thead>
+								<tr>
+									<th>Course Number</th>
+									<th>Title</th>
+									<th>Credits</th>
+								</tr>
+							</thead>
+							<?php
+								for ($i=0;$i<count($student["taking_course"]);$i++){
+									$style_sub = "";
+									$style_tit = "";
+									$style_cred = "";
+									if( $i % 2 != 0) {
+										$style_sub = 'border-left: none; background-color: #c9c9c9;';
+										$style_tit = 'background-color: #c9c9c9;';
+										$style_cred = 'border-right: none; background-color: #c9c9c9;';
+									}
+									echo "<tr>";
+									echo "<td id='left' style='$style_sub'>".$student["taking_course"][$i]['subject'].' '.$student["taking_course"][$i]['catalog'].'</td>';
+									echo "<td style='$style_tit'>".$student["taking_course"][$i]['title'].'</td>';
+									echo "<td id='right' style='$style_cred'>".$student["taking_course"][$i]['cred'].'</td>';
+									echo '</tr>';
+								}
+							?>
+						</table>
+					</div>
 				</div>
-				<div class='table_area'>
-					<table class='taking_table'>
-						<thead>
-							<tr>
-								<th style="border-radius: 10px 0px 0px 0px;">Course Number</th>
-								<th>Title</th>
-								<th style="border-radius: 0px 10px 0px 0px;">Credits</th>
-							</tr>
-						</thead>
-						<?php
-						for ($n = 1; $n <= 2; $n++) {
-							if ($n + 1 == $n) {
-								$style_subject = 'border-left: none; border-bottom: none;';
-								$style_title = 'border-bottom: none;';
-								$style_credit = 'border-right: none; border-bottom: none;';
-							} else {
-								$style_subject = 'border-left: none;';
-								$style_title = '';
-								$style_credit = 'border-right: none;';
-							}
-						}
-						for ($i = 0; $i < count($student["taking_course"]); $i++) {
-							if ($i % 2 == 0) {
-								$style_subject = $style_subject . 'background-color: #c9c9c9';
-								$style_title = $style_title . 'background-color: #c9c9c9';
-								$style_credit = $style_credit . 'background-color: #c9c9c9';
-							}
-							echo "<tr>";
-							echo "<td style='$style_subject'>" . $student["taking_course"][$i]['subject'] . ' ' . $student["taking_course"][$i]['catalog'] . '</td>';
-							echo "<td style='$style_title'>" . $student["taking_course"][$i]['title'] . '</td>';
-							echo "<td style='$style_credit'>" . $student["taking_course"][$i]['cred'] . '</td>';
-							echo '</tr>';
-						}
-						?>
-					</table>
-				</div>
-			</div>
-			<div class='backup' id='backup'>
-				<div class='backup_header'>
-					<h3>Backup Courses</h3>
-				</div>
-				<div class='table_area'>
-					<table class='backup_table'>
-						<thead>
-							<tr>
-								<th style="border-radius: 10px 0px 0px 0px;">Course Number</th>
-								<th>Title</th>
-								<th style="border-radius: 0px 10px 0px 0px;">Credits</th>
-							</tr>
-						</thead>
-						<?php
-						for ($i = 0; $i < count($student["backup_course"]); $i++) {
-							if ($i % 2 != 0) {
-								$style_subject = $style_subject . 'background-color: #c9c9c9';
-								$style_title = $style_title . 'background-color: #c9c9c9';
-								$style_credit = $style_credit . 'background-color: #c9c9c9';
-							}
-							echo "<tr>";
-							echo "<td style='$style_subject'>" . $student["backup_course"][$i]['subject'] . ' ' . $student["backup_course"][$i]['catalog'] . '</td>';
-							echo "<td style='$style_title'.'padding-top: 10px;'>" . $student["backup_course"][$i]['title'] . '</td>';
-							echo "<td style='$style_credit'>" . $student["backup_course"][$i]['cred'] . '</td>';
-							echo '</tr>';
-						}
-						?>
-					</table>
+				<div class='backup' id='backup'>
+					<div class='taking_header'>
+						<h3>Backup Courses</h3>
+					</div>
+					<div class='table_area'>
+						<table class='taking_table'>
+							<thead>
+								<tr>
+									<th>Course Number</th>
+									<th>Title</th>
+									<th>Credits</th>
+								</tr>
+							</thead>
+							<?php
+								for ($i=0;$i<count($student["backup_course"]);$i++){
+									$style_sub = "";
+									$style_tit = "";
+									$style_cred = "";
+									if( $i % 2 != 0) {
+										$style_sub = 'border-left: none; background-color: #c9c9c9;';
+										$style_tit = 'background-color: #c9c9c9;';
+										$style_cred = 'border-right: none; background-color: #c9c9c9;';
+									}
+									echo "<tr>";
+									echo "<td id='left' style='$style_sub'>".$student["backup_course"][$i]['subject'].' '.$student["backup_course"][$i]['catalog'].'</td>';
+									echo "<td style='$style_tit'.'padding-top: 10px;'>".$student["backup_course"][$i]['title'].'</td>';
+									echo "<td id='right' style='$style_cred'>".$student["backup_course"][$i]['cred'].'</td>';
+									echo '</tr>';
+								}
+							?>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	</div> <!-- flexbox div ends -->
+</div> <!-- flexbox div ends -->
 
-	<script>
-		$('nav ul .schedule-show').toggleClass("sch");
-		$('nav ul .first').toggleClass("rotate");
-		$('.schedule-view-btn').css({
-			"color": "#8a0000",
-			"border-left-color": "#8a0000"
-		});
-	</script>
+<script>
+	$('nav ul .schedule-show').toggleClass("sch");
+	$('nav ul .first').toggleClass("rotate");
+	$('.schedule-view-btn').css({"color":"#8a0000","border-left-color":"#8a0000"});
+</script>
 </body>
-
 </html>
