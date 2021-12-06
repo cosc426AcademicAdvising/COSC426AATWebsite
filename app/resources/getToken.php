1a2e3f27d45505e7461bfecef5fc326762eff4e8
quincden <78387066+quincden@users.noreply.github.com>
2021-12-06 15:45:37 -0500

    Corrected auto login for API changes

--- app/resources/getToken.php
+++ app/resources/getToken.php
@@ -1,7 +1,7 @@
 <?php
 
 function getToken(){
-    $usr = array('email' => 'testing@email.com', 'password' => 'test123');
+    $usr = array('id' => 9999999, 'password' => 'suadvisors');
     $url = 'https://cosc426restapi.herokuapp.com/api/user/login/';
     $headers = array('Content-Type' => 'application/json');
     $response = Requests::post($url, $headers, json_encode($usr));
