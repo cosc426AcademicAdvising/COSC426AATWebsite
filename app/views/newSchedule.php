94fbef88d4221317d30df3ad9a5145b14493f9be
Florent Dondjeu Tschoufack <florentdondjeu@yahoo.com>
2021-12-08 20:16:59 -0500

    changed info title page and moved js reference pos

--- app/views/newSchedule.php
+++ app/views/newSchedule.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>Scheduling</title>
 	<meta charset="UTF-8">
@@ -13,40 +14,40 @@
 	<link rel="stylesheet" href="public/css/scheduleNew.css">
 
 	<?php
-		ob_start();
-		session_start();
+	ob_start();
+	session_start();
 
-		// available courses
-		echo '<script> var available_courses = ' . json_encode( getCoursebyRegex("", "", "", ""))  . '; </script>';
+	// available courses
+	echo '<script> var available_courses = ' . json_encode(getCoursebyRegex("", "", "", ""))  . '; </script>';
 
-		//student information
-		$student = getStudent($_SESSION['username']);
-		$progress = $student['credits'];
+	//student information
+	$student = getStudent($_SESSION['username']);
+	$progress = $student['credits'];
 
-		$majors = array();
-		for($i=0;$i<count($student['major']);$i++) {
-			$majors[$i] = $student['major'][$i]['title'];
+	$majors = array();
+	for ($i = 0; $i < count($student['major']); $i++) {
+		$majors[$i] = $student['major'][$i]['title'];
+	}
+	$minors = array();
+	if (count($student['minor']) > 0) {
+		for ($i = 0; $i < count($student['minor']); $i++) {
+			$minors[$i] = $student['minor'][$i]['title'];
 		}
-		$minors = array();
-		if(count($student['minor']) > 0) {
-			for($i=0;$i<count($student['minor']);$i++) {
-				$minors[$i] = $student['minor'][$i]['title'];
-			}
-		}
+	}
 
-		// draft
-		$is_draft_present = isDraftPresent($student['s_id']);
-		echo '<script> var is_draft_present = ' . json_encode($is_draft_present)  . '; </script>';
-		// if draft make js variable for it
-		if ($is_draft_present == 1) {
-			$draft = getDraft($student['s_id']);
-			echo '<script> var student_draft = ' . json_encode($draft)  . '; </script>';
-		}
-		// for recommandations
-		combinedFourYear($majors);
-		echo '<script> var student_id = ' . json_encode($student['s_id'])  . '; </script>';
-		echo '<script> var student_course_hist = ' . json_encode($student['course_taken'])  . '; </script>';
-		echo '<script> var current_semester_number = ' . json_encode(intval($student['semester']))  . '; </script>';
+	// draft
+	$is_draft_present = isDraftPresent($student['s_id']);
+	echo '<script> var is_draft_present = ' . json_encode($is_draft_present)  . '; </script>';
+	// if draft make js variable for it
+	if ($is_draft_present == 1) {
+		$draft = getDraft($student['s_id']);
+		echo '<script> var student_draft = ' . json_encode($draft)  . '; </script>';
+	}
+	// for recommandations
+	combinedFourYear($majors);
+	echo '<script> var student_id = ' . json_encode($student['s_id'])  . '; </script>';
+	echo '<script> var student_course_hist = ' . json_encode($student['course_taken'])  . '; </script>';
+	echo '<script> var current_semester_number = ' . json_encode(intval($student['semester']))  . '; </script>';
 	?>
 </head>
 
@@ -55,7 +56,7 @@
 	<datalist id="recommended_courses"></datalist>
 
 	<script>
-		available_courses = available_courses.filter(function(val){
+		available_courses = available_courses.filter(function(val) {
 			return val["Allowd Unt"] != "999.00";
 		});
 
@@ -75,88 +76,88 @@
 		});
 
 		var text = "";
-		var seperator =  Array(4).fill(' ').join(''); //4 blank space
-		available_courses.forEach( val => text += '<option value="'+ val["Subject"] + " " +$.trim(val["Catalog"])+ seperator + val["Long Title"] + seperator + val["Allowd Unt"] + '">');
-		$('#available_courses').html( text );
+		var seperator = Array(4).fill(' ').join(''); //4 blank space
+		available_courses.forEach(val => text += '<option value="' + val["Subject"] + " " + $.trim(val["Catalog"]) + seperator + val["Long Title"] + seperator + val["Allowd Unt"] + '">');
+		$('#available_courses').html(text);
 	</script>
 
 	<?php
-		include 'nav.php';
+	include 'nav.php';
 	?>
 
-		<div id="content" style="overflow: scroll;">
-			<div class="container">
+	<div id="content" style="overflow: scroll;">
+		<div class="container">
 			<div class="schedule-new">
 				<form action="" id="programplanningworksheet">
 					<div class="std_form">
-						
+
 						<p class="denote" style="font-weight:normal;font-size:12px;text-align:right"><span class="required"> *</span> denotes a required field</p>
 						<div class="std_title">
 							<h2 style="text-align:center; margin: auto;">Program Planning Worksheet</h2>
 						</div>
-							<div class="std_id">
-								<h4 style="margin: auto auto 18px auto; text-align: center;">Personal Info</h4>
+						<div class="std_id">
+							<h4 style="margin: auto auto 18px auto; text-align: center;">Personal Info</h4>
 
-								<label for="studentname">Name</label>
-								<input type="text" id="studentname" name="studentname" size="40" style="margin-right: 35px;" value="<?php echo $student['name'];?>" readonly>
-								<label for="studentid">ID</label>
-								<input type="text" id="studentid" name="studentid" maxlength="7" minlength="7" size="8" value="<?php echo $student['s_id'];?>" readonly>
-							</div>
+							<label for="studentname">Name</label>
+							<input type="text" id="studentname" name="studentname" size="40" style="margin-right: 35px;" value="<?php echo $student['name']; ?>" readonly>
+							<label for="studentid">ID</label>
+							<input type="text" id="studentid" name="studentid" maxlength="7" minlength="7" size="8" value="<?php echo $student['s_id']; ?>" readonly>
+						</div>
 
-							<div class="std_maj">
-								<h4 style="margin: auto auto 18px auto; text-align: center;">Degree Info</h4>
+						<div class="std_maj">
+							<h4 style="margin: auto auto 18px auto; text-align: center;">Degree Info</h4>
 
-								<p style="margin-top:15px;">
-									Major(s):
-									<span id="major">
-										<?php
-										echo "".implode(", ", $majors);
-										?>
-									</span>
-								</p>
-								<p style="margin-top:15px;">
-									Minor(s):
-									<span id="minor">
-										<?php
-										if(count($student['minor']) > 0) {
-											echo "".implode(", ", $minors);
-										}
-										?>
-									</span>
-								</p>
-							</div>
-
-							<div class="std_register">
-								<h4 style="margin: auto auto 18px auto; text-align: center;">Enrollment Info<span class="required">*</span></h4>
-								<label style="margin-right: 25px;">Registering for<span class="required">*</span></label>
-								<input type="radio" name="season" value="Fall" style="margin-left: 10px;" required>
-								<label for="Fall">Fall </label>
-								<input type="radio" name="season" value="Winter" style="margin-left: 10px;" required>
-								<label for="Winter">Winter </label>
-								<input type="radio" name="season" value="Spring" style="margin-left: 10px;" required>
-								<label for="Spring">Spring </label>
-								<input type="radio" name="season" value="Summer" style="margin-left: 10px;" required>
-								<label for="Summer">Summer </label>
-
-								<label for="year" style="margin-left:40px;">Year <span class="required">*</span></label>
-								<select id="year" name="year">
+							<p style="margin-top:15px;">
+								Major(s):
+								<span id="major">
 									<?php
-									$year = date("Y");
-									$year_count = 5;	//number of years forward
-									for( $i=0; $i<$year_count; $i++){
-										echo '<option>'. ($year + $i) . '</option>';
+									echo "" . implode(", ", $majors);
+									?>
+								</span>
+							</p>
+							<p style="margin-top:15px;">
+								Minor(s):
+								<span id="minor">
+									<?php
+									if (count($student['minor']) > 0) {
+										echo "" . implode(", ", $minors);
 									}
 									?>
-								</select>
-								<br>
-								<br>
-								<span>Earned: </span>
-								<input type="text" id="creditearned" name="creditearned" maxlength="3" size="4" value="<?php echo $student['credits'];?>" readonly>
-								<span>credits.</span>
-								<span style="margin-left:80px">Credits</span>
-								<input type="text" id="creditenrolled" name="creditenrolled" size="3" value="0" readonly>
-								<span>currently enrolled in.</span>
-							</div>
+								</span>
+							</p>
+						</div>
+
+						<div class="std_register">
+							<h4 style="margin: auto auto 18px auto; text-align: center;">Enrollment Info<span class="required">*</span></h4>
+							<label style="margin-right: 25px;">Registering for<span class="required">*</span></label>
+							<input type="radio" name="season" value="Fall" style="margin-left: 10px;" required>
+							<label for="Fall">Fall </label>
+							<input type="radio" name="season" value="Winter" style="margin-left: 10px;" required>
+							<label for="Winter">Winter </label>
+							<input type="radio" name="season" value="Spring" style="margin-left: 10px;" required>
+							<label for="Spring">Spring </label>
+							<input type="radio" name="season" value="Summer" style="margin-left: 10px;" required>
+							<label for="Summer">Summer </label>
+
+							<label for="year" style="margin-left:40px;">Year <span class="required">*</span></label>
+							<select id="year" name="year">
+								<?php
+								$year = date("Y");
+								$year_count = 5;	//number of years forward
+								for ($i = 0; $i < $year_count; $i++) {
+									echo '<option>' . ($year + $i) . '</option>';
+								}
+								?>
+							</select>
+							<br>
+							<br>
+							<span>Earned: </span>
+							<input type="text" id="creditearned" name="creditearned" maxlength="3" size="4" value="<?php echo $student['credits']; ?>" readonly>
+							<span>credits.</span>
+							<span style="margin-left:80px">Credits</span>
+							<input type="text" id="creditenrolled" name="creditenrolled" size="3" value="0" readonly>
+							<span>currently enrolled in.</span>
+						</div>
 						<div class="std_courseSearch">
 							<div class="std_search">
 								<h4 style="margin: auto auto 18px auto; text-align: center;">Course Search<span class="required">*</span></h4>
@@ -164,18 +165,18 @@
 									<div style="display:flex; margin-bottom: 10px;">
 										<input list="available_courses" id="coursesearch" name="coursesearch" placeholder="enter a subject, course number, title or credits" style="width:40vw; margin: auto">
 									</div>
-									
+
 									<div class="checkboxes" id="checkboxes" style="display:inline-block; margin-left:20px">
 										<!-- <label for="coursetype">Fulffilment <br> <font size="1">for Major, Minor, Elective</font></label><br> -->
 										<!-- <input type='text' id="coursetype" name="coursetype"> -->
 										<label style="margin-right: 25px;">Fulfillment<span class="required">*</span></label>
 										<input type="checkbox" value="MAJOR" id="majorcourse" class="checkboxaddcourse">
 										<label for="majorcourse" style="margin-right: 25px;">Major</label>
-										<input type="checkbox" value="MINOR" id="minorcourse"class="checkboxaddcourse" style="margin-left: 2px;">
+										<input type="checkbox" value="MINOR" id="minorcourse" class="checkboxaddcourse" style="margin-left: 2px;">
 										<label for="minorcourse" style="margin-right: 25px;">Minor</label>
 										<input type="checkbox" value="ELECTIVE" id="electcourse" class="checkboxaddcourse" style="margin-left: 2px;">
-										<label for="electcourse" >Elective</label>
-										
+										<label for="electcourse">Elective</label>
+
 										<label for="forBackup" style="margin-left: 75px;">Backup Course?<span class="required">*</span></label>
 										<select id="forBackup" name="forBackup">
 											<option>No</option>
@@ -185,7 +186,7 @@
 										<br>
 										<button type='button' onclick="scheduleAddCourse(coursesearch.value, '', false)">Add To Table</button>
 									</div>
-									
+
 								</div>
 							</div>
 						</div>
@@ -243,41 +244,44 @@
 							<br>
 							<input type="submit" value="Send To Advisor" style="padding: 0 20px 0 20px; font-size: 25px; width: 300px;">
 						</div>
-				</div>
-				</div>
-
-				</form>
+					</div>
 			</div>
-		</div>
-		</div>
-</div> <!-- flexbox div ends -->
-<script>
-	$('nav ul .schedule-show').toggleClass("sch");
-	$('nav ul .first').toggleClass("rotate");
-	$('.schedule-new-btn').css({"color":"#8a0000","border-left-color":"#8a0000"});
 
-	if ( $('#schedule-backupcoursetable tbody').children().length == 1) {
-		$('#schedule-backupcoursetable').toggle();
-	}
+			</form>
+		</div>
+	</div>
+	</div>
+	</div> <!-- flexbox div ends -->
 
-	// if draft was not saved recommend courses
-	if (is_draft_present != 1) {
+	<!-- table and buttons functionalities -->
+	<script src="JS/scheduleNewFuncs.js"></script>
+
+	<!-- courses recommandations -->
+	<script src="JS/recommendCourses.js"></script>
+
+	<!-- fill tables with draft obj data if student had saved-->
+	<script src="public/js/fillWithDraft.js"></script>
+	
+	<script>
+		$('nav ul .schedule-show').toggleClass("sch");
+		$('nav ul .first').toggleClass("rotate");
+		$('.schedule-new-btn').css({
+			"color": "#8a0000",
+			"border-left-color": "#8a0000"
+		});
+
+		if ($('#schedule-backupcoursetable tbody').children().length == 1) {
+			$('#schedule-backupcoursetable').toggle();
+		}
+
+		// if draft was not saved recommend courses
+		if (is_draft_present != 1) {
 			recommend_courses(true)
 		} else {
 			fill_with_draft();
 		}
-	}
-
-</script>
-
-<!-- table and buttons functionalities -->
-<script src="JS/scheduleNewFuncs.js"></script>
-
-<!-- courses recommandations -->
-<script src="JS/recommendCourses.js"></script>
-
-<!-- fill tables with draft obj data if student had saved-->
-<script src="public/js/fillWithDraft.js"></script>
+	</script>
 
 </body>
+
 </html>
