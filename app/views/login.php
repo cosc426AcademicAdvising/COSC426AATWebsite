53fe73c6370540ba9542cbf4c530ca4ffe6c03f2
quincden <78387066+quincden@users.noreply.github.com>
2021-12-11 20:30:55 -0500

    Update login.php

--- app/views/login.php
+++ app/views/login.php
@@ -21,15 +21,15 @@ session_start();
 	</header>
 
 	<?php
-	// redict user to dashboard if session still present
-	if (!empty($_SESSION) && !empty($_SESSION['token'])) {
-		$stud = getStudent($_SESSION['username']);
-		$complete_firstTime = count($stud['course_taken']);
-		if ($complete_firstTime < 1)
-			header("Location: firsttime");
-		else
-			header("Location: dashboard");
-	}
+	// // redict user to dashboard if session still present
+	// if (!empty($_SESSION) && !empty($_SESSION['token'])) {
+	// 	$stud = getStudent($_SESSION['username']);
+	// 	$complete_firstTime = count($stud['course_taken']);
+	// 	if ($complete_firstTime < 1)
+	// 		header("Location: firsttime");
+	// 	else
+	// 		header("Location: dashboard");
+	// }
 	// require 'vendor/autoload.php';
 	// include_once 'funcs/StudentFunctions.php';
 	$msg = '';
